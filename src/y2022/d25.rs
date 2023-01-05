5545457de08fa6f73ed0bffcc13056f24433a8c2
Francesco Marangi <2734054+fmarangi@users.noreply.github.com>
2023-01-05 12:54:45 +0100

    Day 2022.25: Full of Hot Air

---
+++ src/y2022/d25.rs
@@ -0,0 +1,56 @@
+const BASE: isize = 5;
+
+pub fn from_snafu(num: &str) -> isize {
+    num.chars()
+        .rev()
+        .enumerate()
+        .fold(0, |v, (i, c)| match c {
+            '=' => v - BASE.pow(i as u32) * 2,
+            '-' => v - BASE.pow(i as u32),
+            _ => v + BASE.pow(i as u32) * c.to_digit(10).unwrap() as isize,
+        })
+}
+
+pub fn to_snafu(mut value: isize) -> String {
+    let mut result = String::new();
+    while value > 0 {
+        match value % BASE {
+            3 => {
+                result = format!("={}", result);
+                value += BASE
+            }
+            4 => {
+                result = format!("-{}", result);
+                value += BASE
+            }
+            v => result = format!("{}{}", v, result),
+        }
+        value /= BASE;
+    }
+    result
+}
+
+pub fn part1(input: &str) -> String {
+    to_snafu(input.split_ascii_whitespace().map(from_snafu).sum())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs::read_to_string;
+
+    #[test]
+    fn test_snafu() {
+        assert_eq!(from_snafu("20"), 10);
+        assert_eq!(from_snafu("2="), 8);
+        assert_eq!(from_snafu("1=11-2"), 2022);
+
+        assert_eq!(to_snafu(2022), "1=11-2")
+    }
+
+    #[test]
+    fn test_puzzle() {
+        let input = read_to_string("input/2022/25.txt").unwrap();
+        assert_eq!(part1(&input), "122-2=200-0111--=200");
+    }
+}
